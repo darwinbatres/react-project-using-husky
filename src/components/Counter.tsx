3f6c737d8155f7ba71a3ff607660d97b36db0924
Darwin Batres <batres.aguilar@gmail.com>
2020-03-26 22:54:15 -0400

    Initial commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = React.useState(0);
+
+  function increaseCounter() {
+    setCounter(counter + 1);
+  }
+
+  function decreaseCounter() {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <div>
+      <p>Hello from Counter component</p>
+      <p>Counter: {counter}</p>
+      <button type="button" onClick={increaseCounter}>
+        Increase ++
+      </button>
+      <button type="button" onClick={decreaseCounter}>
+        Decrease --
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
