be7643230dad2eba67c91edabc4beace0f7a504a
Darwin Batres <batres.aguilar@gmail.com>
2020-03-28 15:29:33 -0400

    Did it work? 5

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,5 +26,22 @@ module.exports = {
       1,
       { extensions: [".js", ".jsx", ".tsx"] },
     ],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
   },
 };
